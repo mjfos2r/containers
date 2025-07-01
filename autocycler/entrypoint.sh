39eb17a6c08fea7dc217efcf1191c1c1fd8ab7bc
Michael J. Foster <50351736+mjfos2r@users.noreply.github.com>
2025-07-01 17:41:45 +0000

    actually fixed now

--- autocycler/entrypoint.sh
+++ autocycler/entrypoint.sh
@@ -13,40 +13,24 @@
 
 set -e
 
-# force a dumb terminal if we're not running as interactive.
-if [[ -z "$TERM" ]]; then
-    export TERM=dumb
-fi
-
-tools=(
-    "canu" "flye" "lja" "metamdbg"
-    "miniasm" "minimap2" "minipolish"
-    "necat" "nextdenovo" "nextpolish"
-    "racon" "raven-assembler" "wtdbg"
-    "plassembler" "unicycler"
-)
-
-# ensure the path contains the conda binaries.
+export TERM="${TERM:-dumb}"
 export PATH="/opt/conda/envs/autocycler/bin:$PATH"
-# and that plassembler_db is set.
-export PLASSEMBLER_DB="/usr/local/share/plassembler/"
+export PLASSEMBLER_DB="/usr/local/share/plassembler/db/"
 
+# if no args specified, print help, list the installed tools, then exit.
 if [[ $# -eq 0 ]]; then
     autocycler --help
     echo -e "\nInstalled Tools:"
     cat /tmp/installed_tools.txt || echo "None found!"
     echo -e "\nMissing Tools:"
     cat /tmp/missing_tools.txt || echo "None missing!"
-    echo -e"\nTool Versions:"
-    for tool in "${tools[@]}"; do
-        if command -v "$tool" &>/dev/null; then
-            version=$("$tool" --version 2>&1 | head -n1)
-            echo "[ PASS ]::[ $tool ]::[ $version ]"
-        else
-            echo "[ FAIL ]::[ $tool ]::[ not found! ]"
-        fi
-    done
+    exit 0
+fi
+
+# otherwise execute the passed command
+
+if [[ $# -eq 1 && "$1" == *" "* ]]; then
+    exec bash -c "$1"
 else
-    # otherwise execute the passed command
     exec "$@"
 fi
