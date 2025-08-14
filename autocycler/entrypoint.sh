898440cc4105621eef20ce7657fad7157e0d0a6b
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-08-14 11:17:48 -0400

    cromwell requires containers to be executed as root.
    Signed-off-by: Michael Foster <50351736+mjfos2r@users.noreply.github.com>

--- autocycler/entrypoint.sh
+++ autocycler/entrypoint.sh
@@ -1,36 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Keep it minimal so it can't interfere with Cromwell's wrapper
 
-# entrypoint for autocycler docker container. Fixes the following:
-#  - If this container is run as interactive, everything is groovy,
-#    however...
-#  - If this container is run non-interactively within a script,
-#    the lack of TERM definition causes problems with some of the assemblers.
-#    ...cough unicycler cough...
-#
-# This entrypoint solves that, it also retains the default behavior
-# written by Ryan Wick in the original:
-# https://github.com/rrwick/Autocycler/tree/main/pipelines/Automated_Autocycler_Bash_script_by_Ryan_Wick
-
-set -e
+# no more set -e
 
 export TERM="${TERM:-dumb}"
 export PATH="/opt/conda/envs/autocycler/bin:$PATH"
 export PLASSEMBLER_DB="/usr/local/share/plassembler/db/"
 
-# if no args specified, print help, list the installed tools, then exit.
+# If no args, show help and exit (still fine without set -e)
 if [[ $# -eq 0 ]]; then
-    autocycler --help
-    echo -e "\nInstalled Tools:"
-    cat /tmp/installed_tools.txt || echo "None found!"
-    echo -e "\nMissing Tools:"
-    cat /tmp/missing_tools.txt || echo "None missing!"
-    exit 0
+  autocycler --help || true
+  echo -e "\nInstalled Tools:";  cat /tmp/installed_tools.txt 2>/dev/null || echo "None found!"
+  echo -e "\nMissing Tools:";    cat /tmp/missing_tools.txt 2>/dev/null || echo "None missing!"
+  exit 0
 fi
 
-# otherwise execute the passed command
-
-if [[ $# -eq 1 && "$1" == *" "* ]]; then
-    exec bash -c "$1"
-else
-    exec "$@"
-fi
+# Always exec the given command/args exactly
+exec "$@"
